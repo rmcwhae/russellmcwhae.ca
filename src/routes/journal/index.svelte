8b1d7ed74631171615b7927d9eb0b37d0964c9ec
Russell McWhae <russell.mcwhae@gmail.com>
2021-07-01 20:33:14 -0700

    Basic list of journal entries

--- src/routes/journal/index.svelte
+++ src/routes/journal/index.svelte
@@ -1,8 +1,39 @@
-<script>
-	// your script goes here
+<script context="module">
+	export async function load({ fetch }) {
+		try {
+			const blog = await fetch('/journal.json');
+			const posts = await blog.json();
+			return { props: { posts } };
+		} catch (error) {
+			console.error(error);
+		}
+	}
 </script>
 
-<p>This is my journal</p>
+<script>
+	export let posts = [];
+</script>
+
+<p>
+	Welcome to my online journal. This is a collection of infrequently published, generally
+	long-winded thoughts on topics that are important to me. In publishing on such a seldom basis, my
+	aim is to prevent information overload and instead provide concise, polished-until-they-shine
+	works that remain relevant beyond staying at the top of a news feed for a few days. Writing is my
+	way of making sense of things, both by exploring subjects from many different angles, and by
+	discussing the ideas of others who have wrestled with similar questions. I hope you’ll join me for
+	a glimpse into my head.
+</p>
+
+<ul>
+	{#each posts as post}
+		<li>
+			<a href={'journal/' + post.slug}>{post.title}</a>
+			{post.description}
+			{post.readingTime}
+			{post.date}
+		</li>
+	{/each}
+</ul>
 
 <style>
 	/* your styles go here */
