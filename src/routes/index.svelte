6bf34fc858c2c6d1146561cdd4b6590ea478e8e7
Russell McWhae <russell.mcwhae@gmail.com>
2021-07-02 07:29:27 -0700

    Updated blurb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to my portfolio</h1>
-<p>This is the homepage.</p>
+<p>
+	I <a href="journal">write</a>, take&nbsp;<a href="photography">photos</a>, print&nbsp;<a
+		href="calendars">calendars</a
+	>, and&nbsp;build&nbsp;<a href="about">websites</a>.
+</p>
