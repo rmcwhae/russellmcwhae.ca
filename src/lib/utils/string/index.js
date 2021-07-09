32d50543048f74000454d7ec105222ba197a3475
Russell McWhae <russell.mcwhae@gmail.com>
2021-07-08 17:16:42 -0700

    Fix for SSR issue

--- src/lib/utils/string/index.js
+++ src/lib/utils/string/index.js
@@ -1,7 +1,8 @@
 export function createTitle(filename) {
-  return filename.substring(0, filename.indexOf('_')).replaceAll('-', ' ')
+	filename = filename.substring(0, filename.indexOf('_'));
+	return filename.replace(/-/g, ' ');
 }
 
 export function extractDate(filename) {
-  return filename.substring(filename.indexOf('_') + 1).replace('-', ' ')
+	return filename.substring(filename.indexOf('_') + 1).replace('-', ' ');
 }
