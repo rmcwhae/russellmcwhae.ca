4328f912bd1197f73d3f3cc4a32eb8381c4923a8
Russell McWhae <russell.mcwhae@gmail.com>
2021-07-14 10:34:59 -0700

    Adds current page highlight on menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,15 +1,45 @@
 <script>
+    import { page } from '$app/stores'
     import Logo from '$lib/components/Logo.svelte'
     import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte'
 </script>
 
 <nav>
-    <a href="/" class="logo"><Logo /></a>
+    <a
+        href="/"
+        aria-current={$page.path === '/' ? 'page' : undefined}
+        class="logo"><Logo /></a
+    >
     <ul>
-        <li><a href="/journal">Journal</a></li>
-        <li><a href="/photography">Photography</a></li>
-        <li><a href="/calendars">Calendars</a></li>
-        <li><a href="/about">About</a></li>
+        <li>
+            <a
+                href="/journal"
+                aria-current={$page.path === '/journal' ? 'page' : undefined}
+                >Journal</a
+            >
+        </li>
+        <li>
+            <a
+                href="/photography"
+                aria-current={$page.path === '/photography'
+                    ? 'page'
+                    : undefined}>Photography</a
+            >
+        </li>
+        <li>
+            <a
+                href="/calendars"
+                aria-current={$page.path === '/calendars' ? 'page' : undefined}
+                >Calendars</a
+            >
+        </li>
+        <li>
+            <a
+                href="/about"
+                aria-current={$page.path === '/about' ? 'page' : undefined}
+                >About</a
+            >
+        </li>
     </ul>
     <ThemeSwitcher />
 </nav>
@@ -26,14 +56,22 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        border-bottom: 1px solid var(--grey);
+        border-bottom: 1px solid var(--light-grey);
         margin-bottom: var(--s1);
     }
-    a {
+    li a {
         text-decoration: none;
         font-size: 0.8em;
         letter-spacing: 0.125em;
         text-transform: uppercase;
+    }
+    a {
+        color: var(--medium-grey);
+        transition: color 0.17s;
+    }
+    a:hover,
+    [aria-current] {
+        color: var(--text-color);
     }
     @media only screen and (max-width: 1024px) {
         nav {
