c0892a408e4722ac27248019b666edb370373122
Russell McWhae <russell.mcwhae@gmail.com>
2021-07-30 14:47:46 -0700

    Fix for running export

--- src/routes/photography/_galleries.js
+++ src/routes/photography/_galleries.js
@@ -1,19 +1,23 @@
-import fs from 'fs';
+import fs from 'fs'
 import * as StringUtils from '$lib/utils/string'
 
 const galleries = fs
-.readdirSync('./static/galleries')
-.filter(folder => folder !== '.DS_Store')
-.sort((a, b) => {
-  return new Date(StringUtils.extractDate(b)) - new Date(StringUtils.extractDate(a))
-})
-.map((filename) => {
-  return {
-    slug: filename,
-    title: StringUtils.createTitle(filename),
-    date: StringUtils.extractDate(filename)
-    // count: 0, TODO implement me
-  };
-});
+    .readdirSync('./static/galleries')
+    .filter((file) => file !== '.DS_Store')
+    .filter((file) => file !== 'manifest.json')
+    .sort((a, b) => {
+        return (
+            new Date(StringUtils.extractDate(b)) -
+            new Date(StringUtils.extractDate(a))
+        )
+    })
+    .map((filename) => {
+        return {
+            slug: filename,
+            title: StringUtils.createTitle(filename),
+            date: StringUtils.extractDate(filename),
+            // count: 0, TODO implement me
+        }
+    })
 
 export default galleries
