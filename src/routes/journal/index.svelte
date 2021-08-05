cbd820de42264be221b247324b613e3f438eb960
Russell McWhae <russell.mcwhae@gmail.com>
2021-08-05 10:45:26 -0700

    Various updates

--- src/routes/journal/index.svelte
+++ src/routes/journal/index.svelte
@@ -24,27 +24,28 @@
     export let posts
 </script>
 
-<div class="flex">
-    <div>
-        {#each posts as { slug, title, description, date, readingTime, category }}
-            <section>
-                <h2>
-                    <a sveltekit:prefetch href={'/journal/' + slug}>{title}</a>
-                </h2>
-                {#if description}
-                    <p>{description}</p>
-                {/if}
-                <div class="uppercase grey">
-                    <Date {date} />
-                    &middot;
-                    <span>{category}</span>
-                    &middot;
-                    <span>{readingTime.text}</span>
-                </div>
-            </section>
-        {/each}
-    </div>
-    <p>
+<!-- <div class="flex"> -->
+<div class="archive">
+    {#each posts as { slug, title, description, date, readingTime, category }}
+        <section>
+            <div class="box" />
+            <h2>
+                <a sveltekit:prefetch href={'/journal/' + slug}>{title}</a>
+            </h2>
+            <div class="uppercase grey sub">
+                <Date {date} />
+                &middot;
+                <span>{category}</span>
+                &middot;
+                <span>{readingTime.text}</span>
+            </div>
+            {#if description}
+                <p>{description}</p>
+            {/if}
+        </section>
+    {/each}
+</div>
+<!-- <p>
         Welcome to my online journal. This is a collection of infrequently
         published, generally long-winded thoughts on topics that are important
         to me. In publishing on such a seldom basis, my aim is to prevent
@@ -54,19 +55,33 @@
         of things, both by exploring subjects from many different angles, and by
         discussing the ideas of others who have wrestled with similar questions.
         I hope you’ll join me for a glimpse into my head.
-    </p>
-</div>
+    </p> -->
 
+<!-- </div> -->
 <style>
+    .box {
+        background: var(--light-grey);
+        height: 1px;
+        width: 100%;
+        margin-bottom: var(--s2);
+    }
     h2 {
-        font-size: 2em;
+        font-size: 1.6em;
         line-height: 1.2em;
     }
     h2 a {
         text-decoration: none;
     }
     section {
-        margin: var(--s2) 0;
+        /* margin: var(--s2) 0; */
+    }
+    .sub {
+        font-size: 0.8em;
+        margin-top: var(--s-2);
+        margin-bottom: var(--s-2);
+    }
+    p {
+        font-size: 1.1em;
     }
     .flex {
         display: flex;
@@ -74,5 +89,13 @@
     .flex > * {
         flex-grow: 1;
         max-width: 50%;
+    }
+    .archive {
+        /* Define the element as a grid container */
+        display: grid;
+        /* Auto-fit as many items on a row as possible without going under 180px */
+        grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+        /* A little spacing between articles */
+        grid-gap: var(--s2);
     }
 </style>
