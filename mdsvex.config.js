f2950428fb0ab7ab36be6186bd4110ce9934a526
Russell McWhae <russell.mcwhae@gmail.com>
2021-08-11 16:05:55 -0700

    Changing packages

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,8 @@
 import path from 'path'
-import find from 'unist-util-find'
-import toc from '@jsdevtools/rehype-toc'
-import rehypeSlug from 'rehype-slug'
 import remarkfootnotes from 'remark-footnotes'
 import readingTime from 'remark-reading-time'
 import autolink from 'rehype-autolink-headings'
+import remarkToc from 'remark-toc'
 
 const config = {
     extensions: ['.svelte.md', '.md', '.svx'],
@@ -13,20 +11,8 @@ const config = {
         dashes: 'oldschool',
     },
 
-    remarkPlugins: [
-        // adds a `readingTime` frontmatter attribute
-        readingTime(),
-        remarkfootnotes,
-        slug,
-    ],
+    remarkPlugins: [remarkToc, readingTime(), remarkfootnotes, slug],
     rehypePlugins: [
-        rehypeSlug,
-        [
-            toc,
-            {
-                customizeTOC,
-            },
-        ],
         [
             autolink,
             {
@@ -46,35 +32,6 @@ function slug() {
         file.data.fm = {
             ...file.data.fm,
             slug,
-            // remove timezone from parsed date
-            // date: file.data.fm.date
-            //     ? new Date(file.data.fm.date).toLocaleDateString()
-            //     : undefined,
         }
-    }
-}
-
-function customizeTOC(toc) {
-    const isEmpty = !find(toc, (node) => node.type === 'text' && !!node.value)
-    const minHeaders = 3
-
-    if (isEmpty || toc.children[0].children.length < minHeaders) {
-        return false
-    }
-
-    const heading = {
-        type: 'element',
-        tagName: 'h2',
-        children: [{ type: 'text', value: 'Contents' }],
-        properties: {
-            className: 'app-toc__heading',
-        },
-    }
-
-    return {
-        type: 'element',
-        tagName: 'aside',
-        properties: { className: 'toc-container' },
-        children: [heading, toc],
     }
 }
