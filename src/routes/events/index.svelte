bc60cfe270c79252fe3783e1d4756bcf8cfadb21
Russell McWhae <russell.mcwhae@gmail.com>
2021-08-28 12:00:20 -0700

    Spacing update

--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -28,7 +28,7 @@
 <style>
     div {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
         grid-gap: var(--s2);
     }
 </style>
