760ec7ee46982d582c74844996942ccca297698a
Russell McWhae <russell.mcwhae@gmail.com>
2021-11-09 22:14:34 -0800

    Trying an opengraph image

--- src/lib/components/base/SEO.svelte
+++ src/lib/components/base/SEO.svelte
@@ -3,6 +3,7 @@
     export let title = ''
 
     const base = 'Russell’s Portfolio'
+    const img = 'https://russellmcwhae.ca/opengraph.png'
 
     $: pageTitle = title ? base + ' » ' + title : base
 </script>
@@ -10,6 +11,9 @@
 <svelte:head>
     <title>{pageTitle}</title>
     <meta name="description" content={description} />
+    <meta property="og:image" content={img} />
+    <meta property="og:title" content={pageTitle} />
+    <meta property="og:description" content={description} />
     <meta
         name="robots"
         content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
