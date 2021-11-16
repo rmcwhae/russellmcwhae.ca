a8093f7472e85f6f80bbe38c93a6cba4e509fbfe
Russell McWhae <russell.mcwhae@gmail.com>
2021-11-15 19:53:35 -0800

    Additional description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,7 +29,7 @@
         {
             href: 'reading-list',
             title: 'Reading List',
-            description: 'Books I have read.',
+            description: 'Books I have read and want to read.',
         },
         {
             href: 'uses',
