e8a000ea4da145f1dfaf616dd27bb777f3772d10
Russell McWhae <russell.mcwhae@gmail.com>
2022-02-23 10:23:03 -0800

    Package updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
     import { posts } from '$lib/services/posts'
 
     export async function load({ fetch }) {
-        const images = await fetch(`/index.json`).then((r) => r.json())
+        const images = await fetch(`/data`).then((r) => r.json())
         const latestPosts = posts.slice(0, 4)
 
         return {
