6cea25bde1da34acceb417260973a6e4bca9db04
Russell McWhae <russell.mcwhae@gmail.com>
2022-03-08 13:56:15 -0800

    Trying a static adapter again

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-// import adapter from '@sveltejs/adapter-static'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-vercel'
 import { isoImport } from 'vite-plugin-iso-import'
 import preprocess from 'svelte-preprocess'
 import imagePreprocessor from 'svimg'
@@ -27,14 +27,9 @@ const config = {
         amp: false,
         prerender: {
             crawl: true,
-            default: true,
             enabled: true,
         },
-        adapter: adapter({
-            // pages: 'build',
-            // assets: 'build',
-            // fallback: null,
-        }),
+        adapter: adapter(),
         vite: {
             ssr: {
                 noExternal: [],
