4acf95edbfe1c3ec4c4930622b0c5da1240aac5f
Russell McWhae <russell.mcwhae@gmail.com>
2022-07-06 09:17:55 -0700

    Package updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-// import adapter from '@sveltejs/adapter-static'
 import adapter from '@sveltejs/adapter-vercel'
-import { isoImport } from 'vite-plugin-iso-import'
 import preprocess from 'svelte-preprocess'
 import imagePreprocessor from 'svimg'
-import path from 'path'
-
-// const { imagetools } = pack
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -29,29 +24,7 @@ const config = {
             default: true,
             enabled: true,
         },
-        adapter: adapter({
-            // pages: 'build',
-            // assets: 'build',
-            // fallback: null,
-        }),
-        vite: {
-            ssr: {
-                noExternal: [],
-            },
-            server: {
-                fs: {
-                    // Allow serving files from one level up to the project root
-                    allow: ['..'],
-                },
-            },
-            resolve: {
-                alias: {
-                    $static: path.resolve('static'),
-                    $root: path.resolve('.'),
-                },
-            },
-            plugins: [isoImport()],
-        },
+        adapter: adapter(),
     },
 }
 
