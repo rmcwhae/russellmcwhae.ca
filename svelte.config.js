ad2a808cc355f88e213c74670bad19d330e7621c
Russell McWhae <russell.mcwhae@gmail.com>
2022-07-25 13:51:52 -0700

    Package updates

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
-import imagePreprocessor from 'svimg'
+import { imagePreprocessor } from 'svimg'
 
 /** @type {import('@sveltejs/kit').Config} */
 
