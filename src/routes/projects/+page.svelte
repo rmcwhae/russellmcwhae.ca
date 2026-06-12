ebea885d53b7e9c1984ff4ba5c79fb181f3abcbf
Russell McWhae <russell.mcwhae@gmail.com>
2026-06-11 21:53:28 -0700

    Updated images

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,25 +5,18 @@
 
     const projects = [
         {
-            title: 'AvCan Mobile App',
-            href: 'https://app.avalanche.ca',
-            description:
-                'The current iteration of the Avalanche Canada Mobile App: a Progressive Web App (PWA) with offline support.',
-            stack: ['PWA', 'Next.js'],
-        },
-        {
             title: 'Avy Savvy',
             href: 'https://avysavvy.ca',
             description:
                 'This is one of the first projects I contributed to at Avalanche Canada, an online tutorial that introduces avalanche risk management to recreational backcountry users.',
-            stack: ['Svelte', 'Sapper', 'Sanity'],
+            stack: ['Next.js', 'Sanity'],
         },
         {
             title: 'Dunn Legal Services',
             description:
                 'An online presence for a Calgary wills and estates lawyer.',
             href: 'https://dunnlegalservices.com',
-            stack: ['Svelte', 'Sapper'],
+            stack: ['HTML'],
         },
         {
             title: 'Pirate Battleship',
@@ -41,16 +34,13 @@
 <h1 class="mb-3">Projects</h1>
 
 <div class="flow">
-    <Project project={projects[0]}>
-        <Image src="/projects/avcanpwa.jpg" alt={projects[0].title} />
+    <Project project={projects[0]} reverse>
+        <Image src="/projects/avysavvy.jpg" alt={projects[0].title} />
     </Project>
-    <Project project={projects[1]} reverse>
-        <Image src="/projects/avysavvy.jpg" alt={projects[1].title} />
+    <Project project={projects[1]}>
+        <Image src="/projects/dunnlegalservices.jpg" alt={projects[1].title} />
     </Project>
-    <Project project={projects[2]}>
-        <Image src="/projects/dunnlegalservices.jpg" alt={projects[2].title} />
-    </Project>
-    <Project project={projects[3]} reverse>
-        <Image src="/projects/piratebattleship.jpg" alt={projects[3].title} />
+    <Project project={projects[2]} reverse>
+        <Image src="/projects/piratebattleship.jpg" alt={projects[2].title} />
     </Project>
 </div>
