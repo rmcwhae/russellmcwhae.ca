44932d860e027fcbe1a723aa47f76be32b04c6fd
Russell McWhae <russell.mcwhae@gmail.com>
2026-07-28 08:21:02 -0700

    Updates

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { resolve } from '$app/paths'
     import JournalEntrySet from '$lib/components/journal/EntrySet.svelte'
     import JournalEntry from '$lib/components/journal/Entry.svelte'
     import SEO from '$lib/components/base/SEO.svelte'
@@ -13,9 +14,11 @@
 
 <div class="restricted-width">
     <p class="mb-2">
-        Welcome to my online journal, a collection of infrequently published,
-        usually long-winded thoughts on topics that are important to me. I hope
-        you’ll join me for a glimpse into my head.
+        Welcome to my online journal, a collection of long-form thoughts on
+        technology, the outdoors, and life. Text generation and ideas are my
+        own, though I do use AI for editing and refining (as of 2025). I hope
+        you’ll join me for a glimpse into my head. Also see my
+        <a href={resolve('/reading-list')}>reading list</a>.
     </p>
     <div class="mb-s0 featured">
         <JournalEntry post={latestPost} featured />
